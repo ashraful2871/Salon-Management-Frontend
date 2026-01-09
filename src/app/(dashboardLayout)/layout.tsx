b6318a662772527f8c88fa23da91cd17b32d55d8
ashraful2871 <ashrafulislam2758@gmail.com>
2026-01-09 21:42:26 +0600

    add comment and push to new branch

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -5,6 +5,7 @@ const CommonDashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen overflow-hidden">
       <DashboardSidebar />
+      {/*dashboard content*/}
       <main className="flex-1 overflow-y-auto bg-gray-50">
         <div className="ml-64 min-h-screen p-6 transition-all duration-300">
           {children}
